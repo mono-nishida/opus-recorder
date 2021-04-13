4ae4bc504079db17ccbad8705d8e0f88eb9d1f42
Christopher Rudmin <chris.rudmin@gmail.com>
2021-04-13 14:06:12 -0400

    Add debug config

---
+++ webpack.debug.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    globalObject: 'typeof self !== \'undefined\' ? self : this',
+    library: 'test',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "debug")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }]
+      }
+    ]
+  }
+};
